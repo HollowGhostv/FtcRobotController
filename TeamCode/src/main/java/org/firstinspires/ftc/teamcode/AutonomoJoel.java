ad507ca8051b8f39664a7e2981511137e938d702
joel <pimpopotter4@gmail.com>
2025-12-09 14:26:41 -0600

    Creacion de un autonomo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomoJoel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomoJoel.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutonomoJoel {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+
+@Autonomous
+
+public class AutonomoJoel extends LinearOpMode
+{
+    DcMotor FL;
+    DcMotor FR;
+    DcMotor BL;
+    DcMotor BR;
+    GoBildaPinpointDriver odo;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        BR = hardwareMap.get(DcMotor.class, "BR");
+
+        {
+            waitForStart();
+            FL.setPower(0.5);
+            FR.setPower(0.5);
+            BL.setPower(0.5);
+            BR.setPower(0.5);
+            sleep(4000);
+
+            FL.setPower(0);
+            FR.setPower(0);
+            BL.setPower(0);
+            BR.setPower(0);
+        }
+    }
 }
