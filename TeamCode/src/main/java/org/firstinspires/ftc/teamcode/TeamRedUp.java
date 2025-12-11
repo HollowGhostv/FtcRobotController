6bdefa49abb6ce08bbb5e383bfb036e0d0b2648e
joel <pimpopotter4@gmail.com>
2025-12-11 12:48:41 -0600

    Creacion de un autonomo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamRedUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamRedUp.java
@@ -76,7 +76,7 @@ public class TeamRedUp extends OpMode
             FR.setPower(0);
             BL.setPower(0);
             BR.setPower(0);
-            Intake2.setPower(1);
+            Intake2.setPower(0.63);
             step1 = true;
             TurnStep2 = true;
         }
