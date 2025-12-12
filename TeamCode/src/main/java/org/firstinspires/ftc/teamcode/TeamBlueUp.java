cd152a1e3aaf20a5146ae246443fa3c40001028e
Lenovo <joseantonio.castillo.5a@gmail.com>
2025-12-12 15:10:07 -0600

    ¡¡

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamBlueUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamBlueUp.java
@@ -87,13 +87,13 @@ public class TeamBlueUp extends OpMode
             Step1 = true;
             ChangeStep2 = true;
         }
-        if (odo.getHeading(AngleUnit.DEGREES) > 5 && !Step2 && ChangeStep2) {
+        if (odo.getHeading(AngleUnit.DEGREES) > -5 && !Step2 && ChangeStep2) {
             FL.setPower(-0.3);
             FR.setPower(0.3);
             BL.setPower(-0.3);
             BR.setPower(0.3);
         }
-        else if (odo.getHeading(AngleUnit.DEGREES) <= 5 && !Step2 && ChangeStep2) {
+        else if (odo.getHeading(AngleUnit.DEGREES) <= -5 && !Step2 && ChangeStep2) {
             FL.setPower(0);
             FR.setPower(0);
             BL.setPower(0);
